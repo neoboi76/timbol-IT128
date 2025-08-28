33ccb4eed675fc4e656f28286434070f6fc0cbf8
neoboi76 <dattimbol@mymail.mapua.edu.ph>
2025-08-28 18:31:18 +0800

    Add Timbol_LE3 codes

---
+++ Timbol_LE3/TimbolLE2/TimbolBlogDB/Stored Procedures/spPosts_List.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_List]
+
+AS
+begin
+	set nocount on;
+
+	SELECT [p].[Id], [p].[Title], [p].[Body], [p].[DateCreated], [u].[Username], [u].[firstName], [u].[LastName]
+	FROM dbo.Posts p
+	INNER JOIN dbo.Users u
+	On p.UserId = u.Id
+end
+RETURN 0
