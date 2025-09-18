a0ab2bed3a3e3528f5580a7f41f84881cd8a744a
neoboi76 <dattimbol@mymail.mapua.edu.ph>
2025-09-18 17:21:28 +0800

    Add Project File for Timbol_LE4

---
+++ Timbol_LE4/TimbolLE2/BlogDataLibrary/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public String UserName { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Password { get; set; }
+
+    }
+}
---
+++ Timbol_LE4/TimbolLE2/BlogTestUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlogTestUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlogTestUI")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("44b5e367-0def-4866-bad3-3bc73bb19f87")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
